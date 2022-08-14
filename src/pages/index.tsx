9a54477d7ec6ce7cf780b91f58e240ca256ddfc9
yurimutti <yuri.mutti@querodelivery.com>
2022-08-14 18:21:52 -0300

    build: implement prettier and configure eslint

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,7 @@
-import type { NextPage } from 'next'
+import type { NextPage } from 'next';
 
 const Home: NextPage = () => {
-  return (
-    <div>HelloWorld</div>
-  )
-}
+  return <div>HelloWorld</div>;
+};
 
-export default Home
+export default Home;
