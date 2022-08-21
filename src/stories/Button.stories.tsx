59a53ed7877c1aef65d4f39a5e1eab3f9409872c
yurimutti <yuri.mutti@querodelivery.com>
2022-08-21 15:45:58 -0300

    build: implement storybook

--- src/stories/Button.stories.tsx
+++ src/stories/Button.stories.tsx
@@ -1,6 +1,5 @@
+import { ComponentMeta, ComponentStory } from '@storybook/react';
 import React from 'react';
-import { ComponentStory, ComponentMeta } from '@storybook/react';
-
 import { Button } from './Button';
 
 // More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
