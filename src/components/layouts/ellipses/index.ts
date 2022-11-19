67259f9b3bf7eb08011aeefd9babe64c269c5bcb
yurimutti <yuri.mutti@querodelivery.com>
2022-11-19 13:24:43 -0300

    feat: restore old bg colors

--- src/components/layouts/ellipses/index.ts
+++ src/components/layouts/ellipses/index.ts
@@ -5,5 +5,5 @@ export const Ellipses = styled('div', {
   position: 'absolute',
   zIndex: -1,
   background:
-    'radial-gradient(circle at 15% 50%, rgba(252, 0, 255, 0.1) , rgba(255, 255, 255, 0) 25%), radial-gradient(circle at 85% 30%, rgba(0, 219, 222, 0.2), rgba(255, 255, 255, 0) 25%)',
+    'radial-gradient(circle at 15% 50%, rgba(238, 58, 136, 0.1) , rgba(255, 255, 255, 0) 25%), radial-gradient(circle at 85% 30%, rgba(128, 162, 245, 0.2), rgba(255, 255, 255, 0) 25%)',
 });
