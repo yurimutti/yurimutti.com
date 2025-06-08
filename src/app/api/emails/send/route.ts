f86d484efb5fa0fc953c112d0d4d0324d34e7da2
Yuri Mutti <muttiyuri@gmail.com>
2025-06-08 13:10:46 +0200

    refactor: resend api

--- src/app/api/emails/send/route.ts
+++ src/app/api/emails/send/route.ts
@@ -2,7 +2,7 @@ import { NextResponse } from 'next/server';
 
 import { Resend } from 'resend';
 
-const resend = new Resend(process.env.NEXT_PUBLIC_RESEND_API_KEY);
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 export async function POST(request: Request) {
   const { email, name, message } = await request.json();
