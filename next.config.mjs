a21d9adaf400d1729b63d754c55ac37d5d1f9b1c
Yuri Mutti <muttiyuri@gmail.com>
2026-07-13 19:06:00 +0200

    feat: add security headers (HSTS, COOP, XFO)

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,23 @@ import remarkSmartypants from 'remark-smartypants';
 /** @type {import('next').NextConfig} */
 const baseConfig = {
   pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Strict-Transport-Security',
+            value: 'max-age=63072000; includeSubDomains; preload',
+          },
+          { key: 'Cross-Origin-Opener-Policy', value: 'same-origin' },
+          { key: 'X-Frame-Options', value: 'DENY' },
+          { key: 'X-Content-Type-Options', value: 'nosniff' },
+          { key: 'Referrer-Policy', value: 'strict-origin-when-cross-origin' },
+        ],
+      },
+    ];
+  },
 };
 
 const withMDX = nextMDX({
